a3a1d03864ffdbdc719220952f264f993e025fba
vartika99 <54948113+vartika99@users.noreply.github.com>
2021-04-09 20:04:01 -0400

    added code for login page

---
+++ src/pages/login.js
@@ -0,0 +1,24 @@
+function validate() {
+    var username = document.getElementById(("username")).value;
+    var password = document.getElementById(("password")).value;
+    var error = "";
+
+    var regex = new RegExp(/[a-zA-Z]+$/);
+    var regex_user = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+
+    if (username.length == 0) {
+        error += "username field can't be left blank \n";
+        alert(error);
+    }
+
+    if (password.length == 0) {
+        error += "password field can't be left blank \n";
+        alert(error);
+    }
+
+    if (!(regex_user.test(username))) {
+        alert("please enter a valid username");
+    }
+
+    location.reload();
+}
