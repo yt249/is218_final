8372a9201338ec26cc93a15a759565b39fea6e27
vartika99 <54948113+vartika99@users.noreply.github.com>
2021-04-12 20:03:20 -0400

    added css for login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 function validate() {
-    var username = document.getElementById(("username")).value;
-    var password = document.getElementById(("password")).value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     var error = "";
 
     var regex = new RegExp(/[a-zA-Z]+$/);
